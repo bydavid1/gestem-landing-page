499e2c250935ff969f5e4b6916d051b3c09e13ec
byronjd <byronjimenez9911@gmail.com>
2020-03-18 17:27:05 -0600

    Contact Us seccion started
    Accent color theme changed

--- js/main.js
+++ js/main.js
@@ -221,4 +221,48 @@ jQuery(document).ready(function($) {
 
 	};
 	siteCarousel();
+
+
+	//------------------------------------------------------------Sticky menu-----------------------------------------------------------------------------
+	var siteSticky = function() {
+		$(".js-sticky-header").sticky({topSpacing:0});
+	};
+	siteSticky();
+
+	// navigation
+  var OnePageNavigation = function() {
+    var navToggler = $('.site-menu-toggle');
+   	$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a", function(e) {
+      e.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        'scrollTop': $(hash).offset().top
+      }, 800, 'easeInOutCirc', function(){
+        window.location.hash = hash;
+      });
+
+    });
+  };
+  OnePageNavigation();
+
+  var siteScroll = function() {
+
+  	
+
+  	$(window).scroll(function() {
+
+  		var st = $(this).scrollTop();
+
+  		if (st > 100) {
+  			$('.js-sticky-header').addClass('shrink');
+  		} else {
+  			$('.js-sticky-header').removeClass('shrink');
+  		}
+
+  	}) 
+
+  };
+  siteScroll();
 });
